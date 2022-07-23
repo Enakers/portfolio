c1e6be7722ddadf8d3f3b4bda91744b56289bc1f
EnakNalla <enak.nalla@gmail.com>
2022-07-23 14:37:02 +0100

    feat: add security headers with next-safe

--- next.config.js
+++ next.config.js
@@ -1,7 +1,33 @@
+const nextSafe = require("next-safe");
+
+console.log(process.env.NODE_ENV);
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'standalone'
+  output: "standalone",
+  headers: () => {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          ...nextSafe({
+            isDev: process.env.NODE_ENV !== "production",
+            contentSecurityPolicy: {
+              "script-src": [
+                "'self'",
+                "'sha256-fDVtD703YIdPFRhb6ZJE/SvcwyA7gZRWfRRM6K6r9EA='"
+              ]
+            }
+          }),
+          {
+            key: "Strict-Transport-Security",
+            value: "max-age=31536000; includeSubDomains; preload"
+          }
+        ]
+      }
+    ];
+  }
 };
 
 module.exports = nextConfig;
